649baafeab8cdf4f4dcbce699804ac2cf00524ec
Muhammad Naqi <naqimng786@gmail.com>
2022-11-11 17:50:22 +0500

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+const HeaderLayout = () => {
+  return (
+    <div className="header_layout">
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+        <li>Feedback</li>
+      </ul>
+    </div>
+  );
+};
+export default HeaderLayout;
